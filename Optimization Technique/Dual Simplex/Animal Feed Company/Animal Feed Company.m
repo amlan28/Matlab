50ba6a2bc469bf4ef7f2616f928167f86321c322
Amlan Nayak <92226639+amlan28@users.noreply.github.com>
2021-10-14 13:27:24 +0530

    Create Animal Feed Company.m

---
+++ Optimization Technique/Dual Simplex/Animal Feed Company/Animal Feed Company.m
@@ -0,0 +1,67 @@
+%name-amlan nayak
+% reg no.-19BCD7143
+% Dual Simplex
+%problem-10
+clc
+clear all
+Variables={'X','Y','S1','S2','S3','Xb'};
+NoofVar=2;
+Cost=[3 8 0 0 0 0];
+Costd=Cost.*(-1);
+A=[-1 -1 ;1 0;0 -1];
+B=[-200;80;-60];
+s=eye(size(A,1));
+X=[A s B];
+BV=NoofVar+1:1:size(X,2)-1;
+zjCj=Costd(BV)*X-Costd;
+zcj=[zjCj;X];
+SimTable=array2table(zcj);
+SimTable.Properties.VariableNames(1:size(zcj,2))=Variables;
+RUN=true;
+while RUN
+Sol=X(:,end);
+if any(Sol<0)
+fprintf('Current solution is not feasible \n')
+[leaVar,pvtRow]=min(Sol);
+fprintf('Leaving row = %d \n',pvtRow)
+Row=X(pvtRow,1:end-1);
+zj=zjCj(:,1:end-1);
+for i=1:size(Row,2)
+if Row(i)<0
+ratio(i)=abs(zj(i)./Row(i));
+else
+ratio(i)=inf;
+end
+end
+[minval,pivCol]=min(ratio);
+fprintf('Entering variable = %d \n',pivCol)
+BV(pvtRow)=pivCol;
+fprintf('Basic Variables(BV) =')
+disp(Variables(BV))
+pvtkey=X(pvtRow,pivCol);
+X(pvtRow,:)=X(pvtRow,:)./pvtkey;
+for i=1:size(X,1)
+if i~=pvtRow X(i,:)=X(i,:)-X(i,pivCol).*X(pvtRow,:);
+end
+end
+zjCj=Costd(BV)*X-Costd; zcj=[zjCj;X];
+SimTable=array2table(zcj);
+SimTable.Properties.VariableNames(1:size(zcj,2))=Variables;
+else
+RUN=false;
+fprintf('Current Basic feasible sol is feasible and optimal \n')
+end
+FinalBFS=zeros(1,size(X,2));
+FinalBFS(BV)=X(:,end);
+FinalBFS(end)=sum(FinalBFS.*Costd);
+OptimalBFS=array2table(FinalBFS);
+OptimalBFS.Properties.VariableNames(1:size(OptimalBFS,2))=Variables;
+end
+
+Minz=FinalBFS(end)*(-1);
+fprintf('Minz=');
+
+disp(Minz)
+  
+  
+#ans=1200  
